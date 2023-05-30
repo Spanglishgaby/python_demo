e962a71c836d8257e87c2e10dced5639ea4d09ac
Yuriko <ygabriela.arnott@gmail.com>
2023-05-30 16:42:42 -0400

    conect to a db

--- app.py
+++ app.py
@@ -1,27 +1,46 @@
-# from flask import Flask
-
-# app = Flask(__name__)
-
-
-# @app.route('/')
-# def index():
-#   return "Hello World"
-
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:abc@localhost:5432/example'
-# db = SQLAlchemy(app)
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:abc@localhost:5432/example'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Person(db.Model):
   __tablename__ = 'persons'
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(), nullable=False)
+
+db.create_all()
+
+@app.route('/')
+
+def index():
+  return 'Hello World!'
+
+# # from flask import Flask
+# # from flask_sqlalchemy import SQLAlchemy
+
+# # app = Flask(__name__)
+# # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:abc@localhost:5432/example'
+# # db = SQLAlchemy(app)
+
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+
+# app = Flask(__name__)
+
+# # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:abc@localhost:5432/example'
+# # db = SQLAlchemy(app)
+
+# # class Person(db.Model):
+# #   __tablename__ = 'persons'
+# #   id = db.Column(db.Integer, primary_key=True)
+# #   name = db.Column(db.String(), nullable=False)
+
+# #   db.create_all()
+
+# @app.route('/')
+#   def index():
+#     return "Hello World"
