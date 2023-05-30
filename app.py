11c4f58a8ead51b16254f3a13ea73e4634672e0a
Yuriko <ygabriela.arnott@gmail.com>
2023-05-30 16:15:53 -0400

    database

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+
+
+# @app.route('/')
+# def index():
+#   return "Hello World"
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-  return "Hello World"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:abc@localhost:5432/example'
+db = SQLAlchemy(app)
