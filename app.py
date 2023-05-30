58f0ad97d29171a430ac07e38c32d04b2b7c4618
Yuriko <ygabriela.arnott@gmail.com>
2023-05-30 16:16:19 -0400

    database2

--- app.py
+++ app.py
@@ -7,9 +7,21 @@
 # def index():
 #   return "Hello World"
 
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:abc@localhost:5432/example'
+# db = SQLAlchemy(app)
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:abc@localhost:5432/example'
 db = SQLAlchemy(app)
+
+class Person(db.Model):
+  __tablename__ = 'persons'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(), nullable=False)
